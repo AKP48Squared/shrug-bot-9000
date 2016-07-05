8c2a2a2e0f172b4267cf34016a0843e4d8f4af5a
Austin Peterson <austin@akpwebdesign.com>
2016-07-05 13:22:13 -0500

    ¯\_(ツ)_/¯

--- main.js
+++ main.js
@@ -14,21 +14,34 @@ ShrugBot9000.prototype.handleCommand = function(context) {
 ShrugBot9000.prototype.handleMessage = function (context) {
   var text = context.text().toLowerCase();
   context.setCustomData('noPrefix', true);
+  var times = {};
 
   if(text.includes('shrug')) {
-    return context.reply(`¯\\_(ツ)_/¯`);
+    if(times.shrug && new Date().getTime() - times.shrug > 15000) {
+      times.shrug = new Date().getTime();
+      return context.reply(`¯\\_(ツ)_/¯`);
+    }
   }
 
   if(text.includes('lenny')) {
-    return context.reply(`( ͡° ͜ʖ ͡°)`);
+    if(times.lenny && new Date().getTime() - times.lenny > 15000) {
+      times.lenny = new Date().getTime();
+      return context.reply(`( ͡° ͜ʖ ͡°)`);
+    }
   }
 
   if(text.includes('lod') || text.includes('disapproval') || text.includes('lookofdisapproval')) {
-    return context.reply(`ಠ_ಠ`);
+    if(times.lenny && new Date().getTime() - times.lenny > 15000) {
+      times.lenny = new Date().getTime();
+      return context.reply(`ಠ_ಠ`);
+    }
   }
 
   if(text.includes('csi') || text.includes('pun') || text.includes('glasses') || text.includes('yeaaaa')) {
-    return context.reply(`(•_•) ( •_•)>⌐■-■ (⌐■_■)`);
+    if(times.glasses && new Date().getTime() - times.glasses > 15000) {
+      times.glasses = new Date().getTime();
+      return context.reply(`(•_•) ( •_•)>⌐■-■ (⌐■_■)`);
+    }
   }
 };
 
